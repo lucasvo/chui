f890b0b7c822e915ae6f4b871f7abd2d102d6c2d
Martin Lundfall <martin.lundfall@protonmail.com>
2019-11-29 02:04:34 +0100

    check inequality with actual numbers

--- src/actions/main.js
+++ src/actions/main.js
@@ -17,7 +17,7 @@ export const save = async function() {
     const saveAmount = web3.utils.toWei(store.get('saveAmount'), 'ether')
     const walletAddress = store.get('walletAddress')
     const allowance = store.get('daiAllowance')
-    if (allowance < saveAmount) {
+    if (Number((allowance) < Number(saveAmount))) {
       return dai.methods.approve(chai.options.address, "-1")
         .send({from: walletAddress})
         .then(function () {
